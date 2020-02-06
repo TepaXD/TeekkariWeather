7d85421e54ba4f9b45c21035fcfac5380f76c39a
TepaXD <46355556+TepaXD@users.noreply.github.com>
2020-02-06 11:53:26 +0200

    Fixing and pushing to Git

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Tekstit.css";
+import "./sijainti.css";
+import { Carousel, CarouselItem } from "react-bootstrap";
+
+class Home extends Component {
+  render() {
+    return (
+      <body className="Sijainti">
+        <h1 className="Transp">l</h1>
+        <h1 className="Tekstit">Welcome to Teekkariweather!</h1>
+      </body>
+    );
+  }
+}
+
+export default Home;
